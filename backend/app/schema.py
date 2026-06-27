f9c367de00da777b6c7eecf8883ded428f6d39f3
OjasSuke <74600224+ojassuke12@users.noreply.github.com>
2026-06-27 08:59:31 +0000

    user schemas and password security

---
+++ backend/app/schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    name: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
+
+    class Config:
+        from_attributes = True
